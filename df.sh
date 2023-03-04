b3d7cf1a4f4d1773c90106a8c6ca4c6490bb915c
timrobins2018 <45065780+timrobins2018@users.noreply.github.com>
2023-03-04 23:02:46 +0800

    Update df.sh

--- df.sh
+++ df.sh
@@ -3,7 +3,7 @@
 while true
 do
     df -h
-    du -sh /workdir/openwrt/*
+    du -sh /workdir/openwrt/* |sort -h
     sleep 60
     echo "==================================================================="
 done
