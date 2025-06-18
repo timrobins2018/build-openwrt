febcfe078bd0f0ec76cbb3cf86311fe397dd1c2e
timrobins2018 <45065780+timrobins2018@users.noreply.github.com>
2025-06-18 20:34:21 +0800

    Update funcs.sh

--- funcs.sh
+++ funcs.sh
@@ -6,7 +6,7 @@ function diskfree() {
         echo "==================================================================="
         df -h
         echo "-------------------------------------------------------------------"
-        du -sh /workdir/openwrt/* |sort -h |tail -n 8
+        du -sh $GITHUB_WORKSPACE/* |sort -h |tail -n 8
         echo "==================================================================="
         sleep 100
     done
@@ -15,20 +15,20 @@ function diskfree() {
 
 function progress() {
     local _pct
-    local _debut
+    local _debug
     local _rowcnt
-    _debut=true
+    _debug=true
     while true
     do
         if [ -f $1 ];then
             _pct=$(cat $1  |tr '\r' '\n' | tail -n 1 |awk '{print $1}')
             _rowcnt=$(cat $1 |tr '\r' '\n'|wc -l)
-            if [ "$_debut" == "true" -a "$_rowcnt" -ge "3" ];then
+            if [ "$_debug" == "true" -a "$_rowcnt" -ge "3" ];then
                 cat $1  2>/dev/null |tr '\r' '\n' | head -n 3
-                _debut=false
+                _debug=false
             fi
             sleep 1
-            if [ "$_debut" == "false" ];then
+            if [ "$_debug" == "false" ];then
                 cat $1  2>/dev/null |tr '\r' '\n' | tail -n 1 |awk '{if ($1 > '${_pct:-0}') print $0}'
             fi
         fi
